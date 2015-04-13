e320b65fdbec739ce2077fd5cbab3a5005dd86a7
Miantang <antisaber@gmail.com>
2015-04-13 11:07:23 +0800

    u

--- mysql/data.sql
+++ mysql/data.sql
@@ -33,10 +33,13 @@ create table  if not exists userlists
 	(
         `userid` int(255) not null  auto_increment primary key,
         `username` char(255) not null,
-        `password` char(255) not null
-	);
-insert into `userlists` (`userid`, `username`, `password`) values
-(1, 'admin', 'admin');
+        `password` char(255) not null,
+        `realname` tinytext not null,
+        `qq` tinytext not null,
+        `email` tinytext not null
+	)DEFAULT CHARSET=utf8;
+insert into `userlists` (`userid`, `username`, `password`, `realname`, `qq`, `email`) values
+(1, 'admin', 'admin', 'da', '10000', 'a@a.com');
 
 create table  if not exists feedBackCode
 (
@@ -50,5 +53,5 @@ insert into `feedBackCode` (`id`, `name`, `code`) values
 (3, 'screen', 1),
 (4, 'tap_water', 1),
 (5, 'projector', 1),
-(6, 'camara', 1);
+(6, 'camara', 1),
 (7, 'led2', 1);
